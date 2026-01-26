7a3cdfd0e1c69ea54a10c5279a0e6f27b3d1cf61
Nik <162503585+MonikAlves@users.noreply.github.com>
2026-01-26 02:37:24 -0300

    telas para preencher

---
+++ Front-End/app/(drawer)/tv.tsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useColorScheme } from '../../hooks/use-color-scheme';
+import { Colors } from '../../constants/theme';
+
+export default function TV() {
+    const theme = useColorScheme() ?? 'light';
+    return (
+        <View style={[styles.container, { backgroundColor: Colors[theme].background }]}>
+            <Text style={[styles.text, { color: Colors[theme].text }]}>TV (Em Breve)</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#121212',
+    },
+    text: {
+        color: '#fff',
+        fontSize: 20,
+    },
+});
